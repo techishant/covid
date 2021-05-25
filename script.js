118d7c8f47d90dbdcf9212532cbb33e1c381ad52
TECHISHANT <74040166+techishant@users.noreply.github.com>
2021-05-25 14:26:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -89,3 +89,27 @@ async function fetchingData() {
   }
 }
 fetchingData();
+
+
+function filterFunction() {
+  // Declare variables
+  var input, filter, tr, td, i, txtValue;
+  input = document.getElementById("myInputState");
+  filter = input.value.toUpperCase();
+  tr = covidTable.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+        tr[0].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+        tr[0].style.display = "";
+      }
+    }
+  }
+}
